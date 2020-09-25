c692044ae653f17af05ff2614c8e4c63f2b68839
Roxana <Roxana@192.168.1.52>
2020-09-25 12:44:19 +0200

    oooo

---
+++ aaaa/src/main/java/demo/AaaaApplication.java
@@ -0,0 +1,13 @@
+package demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AaaaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AaaaApplication.class, args);
+	}
+
+}
